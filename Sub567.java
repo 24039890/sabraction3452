e1e5dfca4c8aeb34f6c12fea05a0acf67810bcb5
24039890 <163397479+24039890@users.noreply.github.com>
2024-03-16 17:02:50 +0200

    Add files via upload

---
+++ Sub567.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sub567;
+
+/**
+ *
+ * @author maceb
+ */
+import java .util.Scanner;
+public class Sub567 {
+
+    public static void main(String[] args) {
+    Scanner entrance = new Scanner(System.in);
+System.out.print("first number entry : ");
+int num1 = entrance.nextInt();
+
+System.out.print("second number entry : ");
+int num2 = entrance.nextInt();
+int subraction = num1 - num2 ;
+        String Sub;
+        
+    System.out.println("the difference between the two numbers is = " + "sub");
+}
+}
